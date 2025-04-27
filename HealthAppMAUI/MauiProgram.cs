c89f942e41458b38b32a59ed7879b4c7d8895abe
Hassan <50418905+mhassxx@users.noreply.github.com>
2025-04-27 05:31:19 +0100

    Add project files.

---
+++ HealthAppMAUI/MauiProgram.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Handlers;
+using Microsoft.Maui.Handlers;
+#if ANDROID
+using Android.Webkit;
+using Android.Net.Http;
+#endif
+
+namespace HealthAppMAUI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+#if ANDROID
+            // 👇 Ignore SSL certificate errors inside WebView (ONLY FOR DEVELOPMENT)
+            Microsoft.Maui.Handlers.WebViewHandler.Mapper.AppendToMapping("AllowSSL", (handler, view) =>
+            {
+                handler.PlatformView.SetWebViewClient(new DevWebViewClient());
+            });
+#endif
+
+            return builder.Build();
+        }
+    }
+
+#if ANDROID
+    // 👇 Custom WebViewClient that ignores SSL errors
+    class DevWebViewClient : WebViewClient
+    {
+       
+    public override void OnReceivedSslError(Android.Webkit.WebView? view, SslErrorHandler? handler, SslError? error)
+{
+    handler?.Proceed(); // Ignore all certificate errors
+}
+    }
+#endif
+}
---
+++ HealthAppMAUI/Platforms/Android/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using AndroidX.Core.View;
+
+
+namespace HealthAppMAUI
+{
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+
+
+
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            var window = this.Window;
+            if (window != null)
+            {
+                if (Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.Lollipop)
+                {
+                    window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#121212"));
+                    window.SetNavigationBarColor(Android.Graphics.Color.ParseColor("#121212"));
+                }
+
+                if (Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.M)
+                {
+                    var decorView = window.DecorView;
+                    var wic = WindowCompat.GetInsetsController(window, decorView);
+                    if (wic != null)
+                    {
+                        // 🔥 Put it right here 🔥
+
+                        // Force WHITE icons on status bar (because background is black)
+                        wic.AppearanceLightStatusBars = false;
+
+                        // OPTIONAL: Keep navigation bar icons adapting to theme
+                        var isDarkTheme = (Resources.Configuration.UiMode & Android.Content.Res.UiMode.NightMask) == Android.Content.Res.UiMode.NightYes;
+                        wic.AppearanceLightNavigationBars = !isDarkTheme;
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+    }
+}
